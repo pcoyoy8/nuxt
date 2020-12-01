6661069a34da9e11a8c0c9d0274347f7fc60d373
Pablo Coyoy <pablo@oaky.com>
2020-12-01 18:21:19 +0100

    First PR

--- components/PersonForm.vue
+++ components/PersonForm.vue
@@ -80,12 +80,7 @@
           md="12"
           sm="12"
         >
-          <!--          Joke text-->
-          <v-textarea
-            v-show="showJokeButton"
-            v-model="jokeText"
-            disabled
-          />
+          Here
         </v-col>
       </v-row>
     </v-container>
